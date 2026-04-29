8f0af8362e297a4b74a5f1ac057ae765d12edb2e
carlosedu <carlos.eduardoss1@outlook.com>
2026-04-28 22:55:06 -0300

    capturando elementos

---
+++ script.js
@@ -0,0 +1,8 @@
+// Key API = d77654c8 
+
+// Capturando os elementos.
+
+const searchFilms = document.getElementById("search")
+const errorMessage = document.getElementById("error-message")
+const nameFilms = document.getElementById("name-films")
+const cardFilms = document.getElementById("card-films")
