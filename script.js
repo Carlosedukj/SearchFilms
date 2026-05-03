2b1eb7b737ebd518417679583ebbf37de8519d0f
carlosedu <carlos.eduardoss1@outlook.com>
2026-05-03 19:30:05 -0300

    corrigindo comentário JS

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// Key API = d77654c8
-
 // Capturando os elementos.
-
 const searchFilms = document.getElementById("search");
 const errorMessage = document.getElementById("error-message");
 const nameFilms = document.getElementById("name-films");
